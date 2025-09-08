94946c81044fb076e07ed70489a1a9863666027f
lofti198 <isolar2005@gmail.com>
2025-09-08 03:52:18 +0200

    invoces suspenced

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
 
-export default function InvoicesPage() {
+async function delay(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+export default async function InvoicesPage() {
+    // await delay(2000);
   return (
     <main className="p-6">
       <h1 className="text-2xl font-bold mb-4">Invoices</h1>
