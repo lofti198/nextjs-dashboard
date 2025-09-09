58d07794b03c0909c11057f71fb8993e913c6e5e
lofti198 <isolar2005@gmail.com>
2025-09-09 03:19:17 +0200

    pagination

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -13,11 +13,13 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const handleSearch = useDebouncedCallback((term) => {
     const params = new URLSearchParams(searchParams);
     console.log(term);
+     params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
+    
     replace(`${pathname}?${params.toString()}`);
   }
 , 1000);
